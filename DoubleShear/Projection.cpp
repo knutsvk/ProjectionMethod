5b342718aef44f7498162388cef7534be5e791fa
knutsvk <ksk38@cam.ac.uk>
2016-11-02 18:26:48 +0000

    Made some changes to working Double Shear code

--- DoubleShear/Projection.cpp
+++ DoubleShear/Projection.cpp
@@ -1,35 +1,44 @@
 #include "Functions.h"
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-    int N; 
+    int N = 50;                 // Number of cells per direction
+    double tStop = 0.8;         // End time for simulation
 
+    // TODO: Make this a function 
     if(argc==1)
     {
         cout << "N: ";
-        cin >> N; 
+        cin >> N;
+        cout << "tStop: ";
+        cin >> tStop;
+    }
+    else if(argc==2)
+    {
+        N = atoi(argv[1]);
+        cout << "tStop: ";
+        cin >> tStop;
     }
     else
     {
-        N = atoi(argv[1]);      // Number of cells per direction
+        N = atoi(argv[1]);
+        tStop = atof(argv[2]);
     }
-    if(N==20) N=21;      
 
     int i, j;                   // Counters for loops
-    double Re = 1.0 / 2.0e-4;  // Reynolds number
-    double dx = 1.0 / N;          // Grid spacing
+    double Re = 1.0 / 2.0e-4;   // Reynolds number
+    double dx = 1.0 / N;        // Grid spacing
     double t = 0.0;             // Time counter
-    double dt = 100 * dx / Re;      // Time step
-    double tStop = 1.8;
+    double dt = 500 * dx / Re;  // Time step
 
-    ofstream fs;           // File stream for writing res
+    ofstream fs;                // File stream for writing res
     char filename[50];
 
     cout << "Incompressible flow in a double shear layer." << endl
-        << "Runtime parameters: N = " << N << ", Re = " << Re 
-        << endl << "Building matrices... " << endl; 
+        << "Runtime parameters: N = " << N << ", Re = " << Re
+        << endl << "Building matrices... " << endl;
 
-    clock_t initiationStart = clock(); 
+    clock_t initiationStart = clock();
 
     // Initiate solution vectors
     VectorXd u = doubleShearLayer(N);
@@ -40,7 +49,7 @@ int main(int argc, char* argv[])
     VectorXd U = doubleShearLayer(N);
     VectorXd V = smallPerturbation(N);
 
-    // Declare load vectors 
+    // Declare load vectors
     VectorXd f_U(N*N);
     VectorXd f_V(N*N);
     VectorXd f_p(N*N);
@@ -54,82 +63,80 @@ int main(int argc, char* argv[])
     solver_UV.compute(Sp_A_UV);
 
     // Build matrix for pressure equations, get solver
-    MatrixXd A_p = buildPressureMatrix(N); 
+    MatrixXd A_p = buildPressureMatrix(N);
     SparseMatrix<double> Sp_A_p = A_p.sparseView();
     SimplicialLDLT<SparseMatrix<double> > solver_p;
     solver_p.compute(Sp_A_p);
 
-    clock_t initiationEnd = clock(); 
+    clock_t initiationEnd = clock();
 
-    cout << "Building of matrices complete. Time elapsed: " 
-        << double(initiationEnd-initiationStart) / CLOCKS_PER_SEC 
-        << " seconds." << endl 
-        << "Advancing in time until final time..." << endl; 
+    cout << "Building of matrices complete. Time elapsed: "
+        << double(initiationEnd-initiationStart) / CLOCKS_PER_SEC
+        << " seconds." << endl
+        << "Advancing in time until final time..." << endl;
 
-    clock_t iterateStart = clock(); 
+    clock_t iterateStart = clock();
 
     int iter = 0;
     while(t < tStop)
     {
-        cout << "iter: " << iter << ", time = " << t << endl; 
-
-        // Make sure we dont go past tStop
-        if( t + dt > tStop )
-            dt = tStop - t; 
+        cout << "iter: " << iter << ", time = " << t << endl;
 
         // Compute rhs for update formula for x-velocity
         updateLoadU(u, v, N, dt, Re, f_U);
 
         // Solve Au * U = bu
         U = solver_UV.solve(f_U);
-        
+
         // Repeat the above for y-velocity
         updateLoadV(u, v, N, dt, Re, f_V);
         V = solver_UV.solve(f_V);
-        
+
         // Compute rhs for update formula for pressure
         updateLoadp(U, V, N, dt, f_p);
 
         // Solve Ap * p = bp
         p = solver_p.solve(f_p);
-        
+
         // Find u and v at next clock step
         updateVelocities(U, V, p, N, dt, u, v);
-        
+
         t += dt;
-        iter++; 
+        iter++;
     }
 
     clock_t iterateEnd = clock();
 
-    cout << "Final time reached after " << iter 
-        << " iterations. " << "Time elapsed: " 
-        << double(iterateEnd-iterateStart) / CLOCKS_PER_SEC 
-        << " seconds." << endl << "Time per iteration: " 
+    cout << "Final time reached after " << iter
+        << " iterations. " << "Time elapsed: "
+        << double(iterateEnd-iterateStart) / CLOCKS_PER_SEC
+        << " seconds." << endl << "Time per iteration: "
         << double(iterateEnd-iterateStart) / CLOCKS_PER_SEC / iter
-        << " seconds." << endl; 
+        << " seconds." << endl;
 
     // Calculate vorticity vector and stream function
     VectorXd omega = buildVorticityVector(u, v, N);
 
-    cout << "Printing results to file... " << endl; 
+    cout << "Printing results to file... " << endl;
 
     // Print results to file
 
-    sprintf(filename, "../Results/omega_N%d.out", N);
+    sprintf(filename, "../Results/omega_N%d_t%d.out", N,
+            static_cast<int>(10*tStop));
     fs.open(filename, std::fstream::out);
     for(i=0; i<N; i++)
     {
         for(j=0; j<N; j++)
         {
-            fs << i*dx << "\t" << j*dx << "\t" 
+            fs << i*dx << "\t" << j*dx << "\t"
                 << omega[i*N+j] << "\n";
         }
         fs << "\n";
     }
     fs.close();
 
-    sprintf(filename, "../Results/uGC_N%d.out", N);
+    sprintf(filename, "../Results/uGC_N%d_t%d.out", N,
+            static_cast<int>(10*tStop));
     fs.open(filename, std::fstream::out);
     i= N/2-1;
     for(j=0; j<N; j++)
@@ -138,7 +145,8 @@ int main(int argc, char* argv[])
     }
     fs.close();
 
-    sprintf(filename, "../Results/vGC_N%d.out", N);
+    sprintf(filename, "../Results/vGC_N%d_t%d.out", N,
+            static_cast<int>(10*tStop));
     fs.open(filename, std::fstream::out);
     for(j=0; j<N; j++)
     {
@@ -146,33 +154,35 @@ int main(int argc, char* argv[])
     }
     fs.close();
 
-    sprintf(filename, "../Results/p_N%d.out", N);
+    sprintf(filename, "../Results/p_N%d_t%d.out", N,
+            static_cast<int>(10*tStop));
     fs.open(filename, std::fstream::out);
     for(i=0; i<N; i++)
     {
         for(j=0; j<N; j++)
         {
-            fs << (i+1)*dx << "\t" << (j+1)*dx << "\t" 
+            fs << (i+1)*dx << "\t" << (j+1)*dx << "\t"
                 << p[i*N+j] << "\n";
         }
         fs << "\n";
     }
     fs.close();
 
-    sprintf(filename, "../Results/vec_N%d.out", N);
+    sprintf(filename, "../Results/vec_N%d_t%d.out", N,
+            static_cast<int>(10*tStop));
     fs.open(filename, std::fstream::out);
     fs << "x\ty\tu\tv\n";
     for(i=0; i<N; i++)
     {
         for(j=0; j<N; j++)
         {
-            fs << i*dx << "\t" << j*dx << "\t" 
+            fs << i*dx << "\t" << j*dx << "\t"
                 << u[i*N+j] << "\t" << v[j*N+i] << "\n";
         }
     }
-    fs.close(); 
+    fs.close();
 
-    cout << "All done. Total runtime: " 
-        << double(clock() - initiationStart) / CLOCKS_PER_SEC 
-        << " seconds. " << endl; 
+    cout << "All done. Total runtime: "
+        << double(clock() - initiationStart) / CLOCKS_PER_SEC
+        << " seconds. " << endl;
 }
